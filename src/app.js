041f76f692d616fc85775541bd3d209babd9df24
Lorenalgm <lorena@proesc.com>
2020-04-20 23:13:47 -0300

    Corrigido teste de likes

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const {id} = request.params;
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
    const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
@@ -40,15 +40,12 @@ app.put("/repositories/:id", (request, response) => {
        return response.status(400).json({ error: 'Repository not found.' });
    }
 
-   if(likes) {
-    return response.status(400).json({ error: 'You cannot update your likes.' });
-   }
-
    const repository = {
        id,
        title,
        url,
-       techs
+       techs,
+       likes: repositories[repositoryIndex].likes
    };
 
    repositories[repositoryIndex] = repository;
